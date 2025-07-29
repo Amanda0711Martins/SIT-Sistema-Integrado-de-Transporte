cea22c4e446c54bd220b3e57d0dd8575c5f6df01
GlauberViniciusCB <g_viniciuscb@outlook.com>
2025-07-29 14:23:21 -0300

    (test) Teste Micro Financeiro

---
+++ back-end/financial/financial-service/src/test/java/com/logistics/financial/repository/BillingRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.logistics.financial.repository;
+
+import org.junit.jupiter.api.Test;
+
+public class BillingRepositoryTest {
+    @Test
+    void testFindByBillingNumber() {
+
+    }
+
+    @Test
+    void testFindByClientId() {
+
+    }
+
+    @Test
+    void testFindByClientIdAndStatus() {
+
+    }
+
+    @Test
+    void testFindByDueDateBefore() {
+
+    }
+
+    @Test
+    void testFindByIssueDateBetween() {
+
+    }
+
+    @Test
+    void testFindByStatus() {
+
+    }
+}
